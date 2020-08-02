46c4a1c6ab8655ee6aafdb142f14828ef828ab15
ltorella <lovencorpse@gmail.com>
2020-08-02 19:34:13 +0800

    CP: Configs for production env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 const todoRoutes = express.Router();
 const PORT = 4000;
 
@@ -80,6 +81,15 @@ todoRoutes.route('/update/:id').post(function(req, res) {
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/v1/todos', todoRoutes);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    // res.sendFile(path.resolve(__dirname, "client", "build", "index.html")); //PROD
+    res.sendFile(path.resolve(__dirname, "client", "index.html"));
+  });
+}
+
+
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
