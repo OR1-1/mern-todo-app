7acc7f166c7d73cd358a11b7ba5f2dc5ce1d1c6f
ltorella <lovencorpse@gmail.com>
2020-08-03 19:20:57 +0800

    CP: Moved routes to sub directory

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
-const todoRoutes = express.Router();
+const todoRoutes = require('./routes/todo')
 const PORT = 4000;
-
-let Todo = require('./models/todo.model');
 
 // DataBase
 // mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true }); // deprecated
@@ -23,60 +21,6 @@ connection.once('open', function() {
   console.log("MongoDB database connection established successfully");
 })
 
-// Routes
-todoRoutes.route('/').get(function(req, res) {
-  Todo.find(function(err, todos) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      res.json(todos)
-    }
-  });
-});
-
-todoRoutes.route('/:id').get(function(req, res) {
-  let id = req.params.id;
-  Todo.findById(id, function(err, todo) {
-    if (!todo)
-      res.status(404).send("data is not found");
-    else
-      res.json(todo);
-  });
-});
-
-todoRoutes.route('/add').post(function(req, res) {
-  let todo = new Todo(req.body);
-  todo.save()
-  .then(todo => {
-    res.status(200).json({'todo': 'todo added successfully'});
-  })
-  .catch(err => {
-    res.status(400).send('adding new todo failed');
-  });
-});
-
-todoRoutes.route('/update/:id').post(function(req, res) {
-  Todo.findById(req.params.id, function(err, todo) {
-    if (!todo)
-      res.status(404).send("data is not found");
-    else {
-      todo.todo_description = req.body.todo_description;
-      todo.todo_responsible = req.body.todo_responsible;
-      todo.todo_priority = req.body.todo_priority;
-      todo.todo_completed = req.body.todo_completed;
-
-      todo.save()
-      .then(todo => {
-          res.json('Todo updated!');
-      })
-      .catch(err => {
-          res.status(400).send("Update not possible");
-      });
-    }
-  });
-});
-
 // App
 app.use(cors());
 app.use(bodyParser.json());
@@ -88,7 +32,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, "client", "index.html"));
   });
 }
-
 
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
